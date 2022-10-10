75af0f1054c8241a332cfda50f139018c54d2674
Nicholas Cutlip <cutlipn@gmail.com>
2022-10-10 14:18:11 -0700

    Add JPanel form to assist with GUI formation

---
+++ CalculatorPanel.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+
+public class CalculatorPanel {
+    private JButton decimalPoint;
+    private JButton a0Button;
+    private JTextArea display;
+    private JButton logButton;
+    private JButton cotButton;
+    private JButton a4Button;
+    private JButton button6;
+    private JButton button7;
+    private JButton a1Button;
+    private JButton a2Button;
+    private JButton a8Button;
+    private JButton a5Button;
+    private JButton cosButton;
+    private JButton button13;
+    private JButton a7Button;
+    private JButton button15;
+    private JButton a3Button;
+    private JButton a6Button;
+    private JButton a9Button;
+    private JButton button19;
+    private JButton button20;
+    private JButton button21;
+    private JButton button22;
+    private JButton sinButton;
+    private JButton tanButton;
+    private JButton lnButton;
+    private JButton button2;
+}
