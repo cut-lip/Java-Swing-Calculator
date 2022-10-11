c27ed30b75307efee08c158e005642de1c35cf3f
Nicholas Cutlip <cutlipn@gmail.com>
2022-10-10 22:39:54 -0700

    Add action listener methods to buttons

--- CalculatorPanel.java
+++ CalculatorPanel.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
 
-public class CalculatorPanel {
+public class CalculatorPanel extends JPanel {
     private JButton decimalPoint;
     private JButton a0Button;
     private JTextArea display;
@@ -28,4 +32,71 @@ public class CalculatorPanel {
     private JButton tanButton;
     private JButton lnButton;
     private JButton button2;
+
+    public CalculatorPanel() {
+        //this.setBackground(Color.green);
+        display.setEditable(false);
+        a1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a2Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a3Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a4Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a5Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a6Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a7Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a8Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a9Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a0Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        display.addComponentListener(new ComponentAdapter() {
+        });
+    }
 }
